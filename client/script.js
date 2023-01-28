cde04dcbdbdbcc083ac2951a93c2aecf2b045b08
Mukesh Shantilal <shantilalmukesh@gmail.com>
2023-01-28 23:08:03 +0530

    api change

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chat-ai-uf9h.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -100,7 +100,6 @@ const handleSubmit = async (e) => {
   if (response.ok) {
     const data = await response.json();
     const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
-
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
